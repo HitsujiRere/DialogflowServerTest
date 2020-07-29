46c213b7cfcad3f3802f4522d4ef9ba8dd3dadcf
HitsujiRere <hitsujirere@gmail.com>
2020-07-29 09:23:57 +0900

    Fix bugs.

--- index.js
+++ index.js
@@ -13,15 +13,17 @@ app.use(bodyParser.json());
 
 const kujis = ['大吉', '中吉', '小吉', '吉'];
 app.post('/dialogflow', (req, res) => {
-    console.log(req.action);
+    console.log(req.body.result);
 
     const kuji = kujis[Math.floor(Math.random() * kujis.length)];
     console.log(`omikuji=${kuji}`);
-    return res.json({
-        speech: `${kuji}を引きました`,
-        displayText: `${kuji}を引きました！`,
-        omikuji: kuji
-    });
+    res.send(
+        JSON.stringify({
+            speech: `${kuji}を引きました`,
+            displayText: `${kuji}を引きました！`,
+            omikuji: kuji
+        })
+    );
 });
 
 app.get('/', (req, res) => {
